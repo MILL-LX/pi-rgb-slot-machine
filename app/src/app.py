ad4cc8dca2750e0f43f0cb2a223d979b2dd04ff0
Mo Mobarak <mo@mobarak.com>
2024-03-14 13:36:21 +0000

    must assemble an image for the whole display from panel images

--- app/src/app.py
+++ app/src/app.py
@@ -2,12 +2,16 @@ import sys
 import time
 
 from display import Display
-from util import test_image_for_display
+import util
 
 def main():
     display = Display()
-    test_image = test_image_for_display(display, num_panels=2)
-    display.setImage(test_image, x_offset=0, y_offset=0)
+    # test_image = util.test_image_for_display(display, num_panels=2)
+    # display.setImage(test_image, x_offset=0, y_offset=0)
+
+    panel_images = util.test_images_for_display(display, num_panels=2) # TODO - replace with a call to generate panels for each animation frame
+    display_image = util.display_image_from_panel_images(panel_images)
+    display.setImage(display_image, x_offset=0, y_offset=0)        
 
     while True:
         try:
--- app/src/util.py
+++ app/src/util.py
@@ -2,6 +2,23 @@ from PIL import Image, ImageDraw
 
 from display import Display
 
+def display_image_from_panel_images(panel_images):
+    num_panels = len(panel_images)
+
+    if num_panels == 0:
+        raise ValueError("panel_images is empty")
+
+    panel_width, panel_height = panel_images[0].size
+
+    display_image = Image.new("RGB", (num_panels * panel_width, panel_height))
+
+    current_x = 0
+    for panel_image in panel_images:
+        display_image.paste(panel_image, (current_x, 0))
+        current_x += panel_width
+
+    return display_image
+
 def test_image_for_panel(display: Display, num_panels: int, fill_color: tuple[int, int, int]):
     panel_width = display.width() // num_panels
     panel_height = display.height()
@@ -11,6 +28,11 @@ def test_image_for_panel(display: Display, num_panels: int, fill_color: tuple[in
     draw.rectangle((0, 0, panel_width * panel_width, panel_height), fill=fill_color)
 
     return image 
+
+def test_images_for_display(display: Display, num_panels: int):
+    colors = [(255,0,0), (0,255,0),(0,0,255),(255,255,255)] # TODO - dynamically generate base on num_panels
+    panel_images = [test_image_for_panel(display, num_panels, colors[panel_number]) for panel_number in range(num_panels)]
+    return panel_images
 
 def test_image_for_display(display: Display, num_panels: int):
     image = Image.new("RGB", (display.width(), display.height()))
