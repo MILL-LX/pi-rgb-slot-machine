eca0bb3a255c322b0b1f8f613bab8e612131b0d2
Mo Mobarak <mo@mobarak.com>
2024-03-19 15:24:24 +0000

    switch to MILL font

--- app/src/slot_machine.py
+++ app/src/slot_machine.py
@@ -32,12 +32,15 @@ class SlotMachine:
         image = Image.new("RGB", (panel_width, panel_height))
         draw = ImageDraw.Draw(image)
 
-        font_size = min(panel_width, panel_height) // 3
-        font = ImageFont.load_default()  # You can use a different font if you prefer
+        font_size = min(panel_width, panel_height)
+        font_path = 'fonts/Canada Type - Screener SC.ttf'
+        font = ImageFont.truetype(font_path, size=font_size)
     
         text_width = draw.textlength(character, font=font)
-        text_height = font_size
-        
+
+        text_bbox = draw.textbbox((0, 0), character, font=font)
+        text_height = text_bbox[3] - text_bbox[1]
+
         x = (panel_width - text_width) // 2
         y = (panel_height - text_height) // 2
     
@@ -52,20 +55,19 @@ class SlotMachine:
 
         self.display.clear()
 
-        # panel_images = util.test_images_for_display(self.display) 
-        current_word = random.choice(self.words)
-        words_minus_current_word = [word for word in self.words if word  != current_word]
-        final_panel_images = [self.panel_image(c) for c in current_word]
+        final_word = random.choice(self.words)
+        final_panel_images = [self.panel_image(c) for c in final_word]
 
+        words_minus_final_word = [word for word in self.words if word  != final_word]
         for i in range(101):
-            current_word = random.choice(words_minus_current_word)
+            current_word = random.choice(words_minus_final_word)
             panel_images = [self.panel_image(c) for c in current_word]
             display_image = util.display_image_from_panel_images(panel_images)
             self.display.setImage(display_image, x_offset=0, y_offset=0)
 
             # Rotate the array of panel images
             # panel_images = panel_images[-1:] + panel_images[:-1]
-            time.sleep(0.03)
+            time.sleep(0.05)
 
         display_image = util.display_image_from_panel_images(final_panel_images)
 
